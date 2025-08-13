7d37e875d27e1626fdcf4a1a7fe98c45494ce5eb
S. M. Sajid Hasan Shanta <sajidhasan673@gmail.com>
2025-08-13 10:20:02 +0600

    Initial Commit

---
+++ SmartTask-OnDeviceAI/SmartTask_OnDeviceAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTask_OnDeviceAIApp.swift
+//  SmartTask-OnDeviceAI
+//
+//  Created by Sajid Shanta on 13/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartTask_OnDeviceAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
